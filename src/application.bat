c288cf8c3211fb0a4680d07ed7615f6216b06bb1
Laurent THOMAS <lth.thomas35@gmail.com>
2015-03-17 13:35:48 +0100

    display copyright

--- src/application.bat
+++ src/application.bat
@@ -1,3 +1,4 @@
- ### application.bat - 201x - Laurent THOMAS ###
+### application.bat - 201x - Laurent THOMAS ###
+echo "Copyright company - 201x"
 echo "Build:10:00"
-echo "version 1"
+echo "version 1.1"
